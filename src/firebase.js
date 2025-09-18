b568fab935289b9e4c6722a5fe1b9c73193fdf66
Druva01 <druva8217@gmail.com>
2025-09-18 23:20:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,16 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyButthPXAxwsmRgVeQ8L2fozujqOpFsENY",
-  authDomain: "internshiphub-51dd7.firebaseapp.com",
-  projectId: "internshiphub-51dd7",
-  storageBucket: "internshiphub-51dd7.firebasestorage.app",
-  messagingSenderId: "719399069026",
-  appId: "1:719399069026:web:057aa0fccda3227771b83b",
-  measurementId: "G-9VDXL4JXL0"
+  authDomain: "internshiphub-51dd7.firebaseapp.com",
+  projectId: "internshiphub-51dd7",
+  storageBucket: "internshiphub-51dd7.firebasestorage.app",
+  messagingSenderId: "719399069026",
+  appId: "1:719399069026:web:057aa0fccda3227771b83b",
+  measurementId: "G-9VDXL4JXL0"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -29,4 +29,5 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
+
 
